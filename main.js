67b7cdc19f17a0a30b88db05626d76c1f0c95949
Fernando Perez <fperez4085@gmail.com>
2023-12-02 09:47:26 -0300

    Renombre archivo app.js por main.js

--- main.js
+++ main.js
@@ -1,18 +1,18 @@
 const objetoDeposito = new Deposito();
 /** Recordar comentar los console.log() al finalizar */
-console.log(objetoDeposito.deposito, " linea 3");
+//console.log(objetoDeposito.deposito, " linea 3");
 objetoDeposito.renderizarProductos(objetoDeposito.deposito);
 /** Ordenar producto por precio */
 const ordenarPorPrecio = (e) => {
     let resultado;
     const eventoBtn = e.target.dataset.id;
-    console.log(eventoBtn, " eventoBtn linea 289")
+    //console.log(eventoBtn, " eventoBtn linea 289")
     if (eventoBtn === "descendente") {
         resultado = objetoDeposito.ordenarProductosPorPrecioDescendente();
     } else {
         resultado = objetoDeposito.ordenarProductosPorPrecioAscendente();
     }
-    console.log(resultado);
+    //console.log(resultado);
     objetoDeposito.renderizarProductos(resultado);
 }
 function ordenarproducto() {
@@ -28,34 +28,35 @@ ordenarproducto();
 const buscarProducto = () => {
     const inputProd = document.getElementById("input-producto-filtrado");
     const inputNombre = inputProd.value.toLowerCase();
-    console.log(inputNombre, " nombreProducto linea 33");
+    ///console.log(inputNombre, " nombreProducto linea 33");
     productoObtenidoPorNombre = objetoDeposito.buscarProductoPorNombre(inputNombre);
-    console.log(productoObtenidoPorNombre);
+    ///console.log(productoObtenidoPorNombre);
     objetoDeposito.renderizarProductos(productoObtenidoPorNombre);
 }
 function agregarEventoBtnBuscarProducto() {
     const botonBuscarprod = document.getElementById("button-producto-filtrado");
     botonBuscarprod.addEventListener("click", buscarProducto);
 }
-agregarEventoBtnBuscarProducto();
+agregarEventoBtnBuscarProducto(); 
 /** Fin buscar producto por nombre */
+/////////////////////////////////////
 /** Ordenar alfabeticamnete */
-const ordenarAlfabeticamente = (e)=>{
+const ordenarAlfabeticamente = (e) => {
     const eventoBtn = e.target.dataset.id;
-    console.log(eventoBtn);
+    //console.log(eventoBtn);
     let orden;
-    if(eventoBtn === "a"){
+    if (eventoBtn === "a") {
         orden = objetoDeposito.ordenarProductoDeAZ();
-        console.log(orden);
-    }else {
+        //console.log(orden);
+    } else {
         orden = objetoDeposito.ordenarProductoDeZA();
-        console.log(orden);
+        //console.log(orden);
     }
     objetoDeposito.renderizarProductos(orden);
 }
 function agregarEventoBtnPorOrdenAlfabetico() {
     const btnOrdenAlfabetico = document.getElementsByClassName("btn-orden-alfabetico");
-    for(const ordenAlfabetico of btnOrdenAlfabetico){
+    for (const ordenAlfabetico of btnOrdenAlfabetico) {
         ordenAlfabetico.addEventListener("click", ordenarAlfabeticamente);
     }
 }
